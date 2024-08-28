52ec62c0f878cc868ab6df898d004bf97a638ed5
the nothing <mydeadass1568@gmail.com>
2024-08-28 14:15:11 +0000

    fixed

---
+++ core/src/slides.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import { Presentation } from '@components';
+</script>
+
+<Presentation>
+	<div class="reveal">
+		<div class="slides">
+			<section>
+				<section data-background-image="https://media1.tenor.com/m/M2Hn73puxJIAAAAC/call-police-calling.gif">
+				  <h1 style="font-size: 3rem; text-transform: none; color: black;">PERLAWANAN BANGSA INDONESIA TERHADAP KOLONIALISME & IMPERIALISME</h1>
+				</section>
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/4/4f/Raden_Saleh_-_Diponegoro_arrest.jpg">
+				  <h1 style="font-size: 3rem; text-transform: none; color: whitesmoke;">Perang Jawa</h1>
+				  <h1 style="font-size: 3rem; text-transform: none; color: whitesmoke;">1825-1830</h1>
+				</section>
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/4/4f/Raden_Saleh_-_Diponegoro_arrest.jpg">
+				  <div class="absolute w-2/5 right-0 shadow-md bg-black bg-opacity-90 text-white p-5 text-lg text-left">
+					<h2>
+						<a class="text-blue-500 hover:text-blue-700" href="https://en.wikipedia.org/wiki/The_Arrest_of_Pangeran_Diponegoro">
+						  Gevangenname van Prins Diponegoro
+						</a>
+					</h2>
+					<p>Ini adalah sebuah lukisan yang menggambarkan penangkapan Pangeran Diponegoro oleh pelukis Raden Saleh sewaktu ia berkuliah di Belanda. Dilukis di tahun 1857, walaupun ia belum pernah bertemu dengan Diponegoro, akan tetapi keluarga si pelukis memiliki hubungan yang sangat erat dengan si Pangeran</p>
+				  </div>
+				</section>
+				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+				  </div>
+				</section>            
+				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
+					  Perselisihan tanah
+					</div>
+					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
+					  Eksploitasi ekonomi & pajak
+					</div>
+					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
+					  Dukungan popular
+					</div>
+					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
+					  Marjinalisasi politik
+					</div>
+				  </div>
+				  <h2 class="mt-5" style="color: black;">Faktor terjadinya perang</h2>
+				</section>        
+				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+				  </div>
+				</section>            
+				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
+					  Kehilangan nyawa
+					</div>
+					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
+					  Represi politik
+					</div>
+					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
+					  Biaya mahal
+					</div>
+					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
+					  Ekspansi tanam paksa
+					</div>
+				  </div>
+				  <h2 class="mt-5" style="color: black;">Efek dari perang</h2>
+				</section>
+			  </section>
+			</div>
+	  
+			<div class="slides">
+			  <section>
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Samalanga_1878.jpg/800px-Samalanga_1878.jpg">
+				  <h1 style="font-size: 3rem; text-transform: none; color: white;">Perang Aceh</h1>
+				  <h1 style="font-size: 3rem; text-transform: none; color: white;">1873–1904</h1>
+				</section>
+				<section data-background-image="https://media1.tenor.com/m/fpsMOKkXUvMAAAAC/pink-poo-maja.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
+					  Lokasi strategis
+					</div>
+					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
+					  Solidaritas Islam
+					</div>
+					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
+					  Manuver politik
+					</div>
+					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
+					  Pantang menyerah
+					</div>
+				  </div>
+				  <h2 class="mt-5" style="color: black;">Faktor terjadinya perang</h2>
+				</section>        
+				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
+				  </div>
+				</section>            
+				<section data-background-image="https://media1.tenor.com/m/EMqsl3xrxqwAAAAd/nameless12-korea.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+				  <div class="flex justify-center">
+					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
+					  Kehilangan nyawa
+					</div>
+					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
+					  Represi politik
+					</div>
+					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
+					  Biaya mahal
+					</div>
+					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
+					  Ekspansi tanam paksa
+					</div>
+				  </div>
+				  <h2 class="mt-5" style="color: black;">Efek dari perang</h2>
+				</section>
+				<section data-background-image="https://media1.tenor.com/m/3_6QXFlSRTMAAAAd/ulzzang-aesthetic.gif">
+			  </section>
+		</div>
+	</div>
+</Presentation>

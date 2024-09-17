7edcfb65bc5a7704d824264f09b9b3a534c49d4a
the nothing <mydeadass1568@gmail.com>
2024-09-17 04:20:43 +0000

    done

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -1,131 +1,92 @@
 <script lang="ts">
 	import { Presentation } from '@components';
+	import Criminal from "./public/war criminal.jpg";
 </script>
 
 <Presentation>
 	<div class="reveal">
 		<div class="slides">
 			<section>
-				<section data-background-image="https://media1.tenor.com/m/M2Hn73puxJIAAAAC/call-police-calling.gif">
-				  <h1 style="font-size: 3rem; text-transform: none; color: black;">PERLAWANAN BANGSA INDONESIA TERHADAP KOLONIALISME & IMPERIALISME</h1>
+				<section data-background-image="https://pbs.twimg.com/media/GVlUy37boAA6-QH.jpg">
+					<h1 style="font-size: 4rem; color: black">Indonesia pada masa Demokrasi liberal hingga Demokrasi terpimpin</h1>
 				</section>
-				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/4/4f/Raden_Saleh_-_Diponegoro_arrest.jpg">
-				  <h1 style="font-size: 3rem; text-transform: none; color: whitesmoke;">Perang Jawa</h1>
-				  <h1 style="font-size: 3rem; text-transform: none; color: whitesmoke;">1825-1830</h1>
+				<section data-background-image="https://histlit.fas.harvard.edu/sites/projects.iq.harvard.edu/files/styles/os_files_xxlarge/public/histlit/files/screen_shot_2019-11-26_at_8.59.29_am.png?m=1574776819&itok=lZyW2vi3" />
+				<section data-background-image="https://miro.medium.com/v2/resize:fit:1400/0*u-K6cRlTQc1sVO3W.jpg" />
+				<section data-background-image="https://miro.medium.com/v2/resize:fit:1400/0*u-K6cRlTQc1sVO3W.jpg">
+					<q style="font-size: 3rem; color: black">Berbeda dengan perang tradisional, perang ini tidak melibatkan pertempuran langsung dalam skala besar antara kedua negara adikuasa, tetapi ditandai dengan perang proksi, perlombaan senjata nuklir, spionase, propaganda, dan persaingan ideologis untuk mendapatkan pengaruh global</q>
 				</section>
-				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/4/4f/Raden_Saleh_-_Diponegoro_arrest.jpg">
-				  <div class="absolute w-2/5 right-0 shadow-md bg-black bg-opacity-90 text-white p-5 text-lg text-left">
-					<h2>
-						<a class="text-blue-500 hover:text-blue-700" href="https://en.wikipedia.org/wiki/The_Arrest_of_Pangeran_Diponegoro">
-						  Gevangenname van Prins Diponegoro
-						</a>
-					</h2>
-					<p>Ini adalah sebuah lukisan yang menggambarkan penangkapan Pangeran Diponegoro oleh pelukis Raden Saleh sewaktu ia berkuliah di Belanda. Dilukis di tahun 1857, walaupun ia belum pernah bertemu dengan Diponegoro, akan tetapi keluarga si pelukis memiliki hubungan yang sangat erat dengan si Pangeran</p>
-				  </div>
+				<section data-background-image="https://origins.osu.edu/sites/default/files/inline-images/2023-04-14%20%281%29.png" />
+				<section data-background-image="https://politiikasta.fi/wp-content/uploads/2020/09/INF3-1295-Marshall-Aid-cartoon-2.jpg" />
+				<section data-background-image="https://assets3.cbsnewsstatic.com/hub/i/r/2023/07/11/b0f698b0-b3cb-4a9e-ac73-c43054c3690f/thumbnail/640x360/8c3784fe347c9bf7eff6c8a47f7b7ad1/nato-member-map-7.jpg?v=631cf5f2b4e8db7f9bc428589402864d">
+					<h2 style="font-size: 3rem; color: black;">NATO</h2>
+					<p style="font-size: 3rem; color: black;">North Atlantic Treaty Organization</p>
 				</section>
-				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-				  </div>
-				</section>            
-				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
-					  Perselisihan tanah
-					</div>
-					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
-					  Eksploitasi ekonomi & pajak
-					</div>
-					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
-					  Dukungan popular
-					</div>
-					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
-					  Marjinalisasi politik
-					</div>
-				  </div>
-				  <h2 class="mt-5" style="color: black;">Faktor terjadinya perang</h2>
-				</section>        
-				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-				  </div>
-				</section>            
-				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
-					  Kehilangan nyawa
-					</div>
-					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
-					  Represi politik
-					</div>
-					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
-					  Biaya mahal
-					</div>
-					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
-					  Ekspansi tanam paksa
-					</div>
-				  </div>
-				  <h2 class="mt-5" style="color: black;">Efek dari perang</h2>
+				<section data-background-image="https://cdn.britannica.com/62/64262-050-B5A6C575/Cold-War-membership-Europe-Soviet-Union-satellites.jpg">
+					<h2 style="font-size: 4rem; color: black">Pakta Warsawa</h2>
 				</section>
-			  </section>
-			</div>
-	  
-			<div class="slides">
-			  <section>
-				<section data-background-image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Samalanga_1878.jpg/800px-Samalanga_1878.jpg">
-				  <h1 style="font-size: 3rem; text-transform: none; color: white;">Perang Aceh</h1>
-				  <h1 style="font-size: 3rem; text-transform: none; color: white;">1873–1904</h1>
+				<section data-background-image="https://mmc.tirto.id/image/2018/03/10/khruschev-sukarno--life.jpg" />
+			</section>
+			<section>
+				<section data-background-image="https://media1.tenor.com/m/5e_9fzGTWxcAAAAd/space-x-falcon-landing.gif">
+					<h1 style="font-size: 4rem;">Teknologi</h1>
+					<p style="color: black;">Industri Peroketan</p>
 				</section>
-				<section data-background-image="https://media1.tenor.com/m/fpsMOKkXUvMAAAAC/pink-poo-maja.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
-					  Lokasi strategis
-					</div>
-					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
-					  Solidaritas Islam
-					</div>
-					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
-					  Manuver politik
-					</div>
-					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
-					  Pantang menyerah
-					</div>
-				  </div>
-				  <h2 class="mt-5" style="color: black;">Faktor terjadinya perang</h2>
-				</section>        
-				<section data-background-image="https://media1.tenor.com/m/kLek8Kd9GIUAAAAC/neural-network-sigmoid.gif" class="transition-all" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box2" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box3" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-					<div data-id="box4" class="bg-gray-600 w-12 h-12 m-2 rounded-md"></div>
-				  </div>
-				</section>            
-				<section data-background-image="https://media1.tenor.com/m/EMqsl3xrxqwAAAAd/nameless12-korea.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-				  <div class="flex justify-center">
-					<div data-id="box1" data-auto-animate-delay="0" class="bg-cyan-500 w-70 h-24 m-2">
-					  Kehilangan nyawa
-					</div>
-					<div data-id="box2" data-auto-animate-delay="0.1" class="bg-pink-500 w-70 h-36 m-2">
-					  Represi politik
-					</div>
-					<div data-id="box3" data-auto-animate-delay="0.2" class="bg-yellow-500 w-70 h-24 m-2">
-					  Biaya mahal
-					</div>
-					<div data-id="box4" data-auto-animate-delay="0.3" class="bg-orange-950 w-70 h-32 m-2">
-					  Ekspansi tanam paksa
-					</div>
-				  </div>
-				  <h2 class="mt-5" style="color: black;">Efek dari perang</h2>
+				<section data-background-image="https://mf.b37mrtl.ru/rbthmedia/images/2019.03/article/5c9cbf9f85600a18d643c492.jpg" />
+				<section data-background-image="https://mf.b37mrtl.ru/rbthmedia/images/2019.03/article/5c9cbf9f85600a18d643c492.jpg">
+					<h1 style="color: black; font-size: 4rem">Юрий Алексеевич Гагарин</h1>
 				</section>
-				<section data-background-image="https://media1.tenor.com/m/3_6QXFlSRTMAAAAd/ulzzang-aesthetic.gif">
-			  </section>
+				<section data-background-image="https://space.skyrocket.de/img_sat/sputnik-1__1.jpg">
+					<h1 style="color: whitesmoke; font-size: 4rem">Sputnik 1</h1>
+				</section>
+				<section data-background-image="https://ilmugeografi.com/wp-content/uploads/2019/07/7c943f58-buzz-flag.jpg">
+					<div style="position: left; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>The Eagle has Landed 🥳🥳🥳</h2>
+						<p>Ini adalah foto dari astronot Buzz Aldrin hormat ke bendera Amerika di Bulan. Buzz Aldrin tergabung dalam misi Apollo 11, yang dimana ia beserta Astronot Neil Armstrong menjadi manusia pertama yang menginjakkan kaki di Bulan</p>
+					</div>
+				</section>
+				<section data-background-image="https://www.popsci.com/wp-content/uploads/2019/07/19/RX4EZMET4KZLMC3O6CTFVXN5KQ.jpg">
+					<div style="position: left; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<p>Dan ini adalah jejak sepatu yang ditinggalkan oleh astronot di misi yang sama, Apollo 11</p>
+					</div>
+				</section>
+				<section data-background-image="https://i0.wp.com/apollo11space.com/wp-content/uploads/2023/04/Untitled-design-2023-04-30T085706.411-1024x576.webp?resize=1024%2C576&ssl=1">
+					<div style="position: justified; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>Saturn 5</h2>
+						<p>Ini adalah roket terbesar yang dibuat oleh NASA. Yaitu Saturn 5. Roket ini yang membawa astonot Buzz Aldrin, Neil Armstrong menuju ke Bulan</p>
+					</div>
+				</section>
+				<section data-background-image="https://cdn.mos.cms.futurecdn.net/jt3aonWDALfwfisswSMprk.jpg">
+					<div style="position: justified; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>Ракета-носитель</h2>
+						<p>Dan ini, adalah roket N-1. Ini adalah roket yang seharusnya menjadi kompetitor Saturn 5. Pemerintah Soviet memutuskan untuk menghentikan pengembangan roket ini dikarenakan roket ini mengalami kecacatan dalam hal penerbangan</p>
+					</div>
+				</section>
+				<section data-background-image="https://gorus21.com/wp-content/uploads/2021/03/Pinochet_-_Kissinger2.jpg">
+					<h1 style="font-size: 4rem; color: white">Operasi Condor</h1>
+				</section>
+				<section data-background-image={Criminal} />
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg" />
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg">
+					<h1 style="font-size: 4rem; color: white">Perang Dingin kenapa ada hubungannya dengan Anjing?</h1>
+				</section>
+				<section data-background-image="https://media.newyorker.com/photos/59fb65a52d0aaf4a81445779/master/w_2560%2Cc_limit/Wellerstein-Laika.jpg">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>лайка</h2>
+						<p>Perkenalkan, Laika. Laika adalah seekor anjing jalanan yang diambil oleh ilmuwan Soviet di jalanan kota Moskow. Laika dijadikan sebagai subjek eksperimen yang dilakukan oleh ilmuwan Soviet untuk mengetahui seberapa lama hewan bisa bertahan hidup di luar angkasa. Alhasil, hasil tes yang dilakukan sangatlah memuaskan bagi Ilmuwan Soviet, Laika bisa bertahan hidup di luar angkasa (dengan bantuan tabung oksigen dan anesthesia) selama 4 jam. Akan tetapi Laika harus mendekam di kabin Sputnik 2 yang sempit. Laika mengalami depresi berat dan harus menghembuskan nafas terakhir karena depresi berat dan juga mati terbakar. Mati terbakar karena kabin Sputnik 2 tidak bisa membendung panasnya atmosfer bumi</p>
+					</div>
+				</section>
+			</section>
+			<section>
+				<section data-background-image="https://pbs.twimg.com/media/EaxjesBXsAEm5iP.jpg">
+					<h1 style="font-size: 4rem; color: white">Perjanjian SALT 1 & 2</h1>
+				</section>
+				<section data-background-image="https://media.meer.com/attachments/58ed7347690d8cc372e8e9046659751057d3446e/store/fill/1090/613/12ba7f90be3005e247c265ed9f5c93ab8efd6ec6035b73e4566929036e76/World-leaders-Shri-Jawaharlal-Nehru-of-India-Kwame-Nkrumah-of-Ghana-Gamal-Abdel-Nasser-of-Egypt.jpg">
+					<h1 style="font-size: 4rem; color: white">Konferensi Tingkat Tinggi Non-blok</h1>
+				</section>
+				<section data-background-image="https://www.vietnamfulldisclosure.org/wp-content/uploads/2020/01/BANDUNG1955.jpg">
+					<h1 style="font-size: 4rem; color: white">Konferensi Asia-Afrika</h1>
+				</section>
+			</section>
 		</div>
 	</div>
 </Presentation>

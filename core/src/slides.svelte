8e526d7ac0610ebef9f3f31e1b4431e42d139ae1
the nothing <mydeadass1568@gmail.com>
2024-09-24 13:04:16 +0000

    nyess

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -8,12 +8,14 @@
 	import lel from './public/lel.jpg';
 	import troll from './public/troll.jpg';
 	import hihi from './public/hihi.jpg';
+	import lmao from './public/lmao.png';
 </script>
 
 <Presentation>
 	<div class="reveal">
 		<div class="slides">
 			<section>
+				<!--this is the start of sub-bab 2 pt.1-->
 				<section data-background-image={Speech}>
 					<h1 style="font-size: 4rem; color: black">Ketersebaran Kekuatan & Identitas Nasional Baru</h1>
 				</section>
@@ -102,10 +104,46 @@
 				</section>
 				<section data-background-image="https://ichef.bbci.co.uk/images/ic/1024x576/p09x75rz.jpg">
 					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Propaganda yang berpusat pada "penyimpangan seksual anggota Gerwani", serta penggambaran PKI sebagai ateis dan anti-nasionalis tak hanya memicu pada pembunuhan massal orang-orang berhaluan kiri, tapi juga penghancuran gerakan perempuan progresif di Indonesia</p>
+						<p>Pada awalnya, organisasi ini terdiri dari aktivis perempuan yang dimana ingin menyuarakan hak-hak perempuan agar mendapat perlakuan setara seperti laki-laki. Mungkin pada awalnya, organisasi ini mendapat dukungan luas dari masyarakat, akan tetapi mengingat organisasi memiliki hubungan dengan PKI, organisasi ini dibungkam dan beberapa anggota nya berakhir dipenjara di kamp konsentrasi era orde baru</p>
 					</div>
 				</section>
 			</section>
+
+			<section>
+				<section data-background-image="https://images.bisnis.com/upload/img/demo_RUU_Pilkada_di_DPR.jpg">
+					<h1 style="font-size: 4rem; color: black">Pemilihan Umum</h1>
+				</section>
+				<section data-background-image={lmao}>
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2 style="font-size: 3rem;">Results</h2>
+						<p>
+							There was a large gap between the "big 4" (PNI, Masjumi, NU and PKI, with more than three-quarters of the vote shared among them) and the rest of the parties, but contrary to expectations, the number of parties actually increased - there were now 28 with seats in the legislature as opposed to 20 before the election, with the largest party holding only 22% of seats
+							In his opening speech, President Sukarno called for an Indonesian form of democracy, and over the next few years, he would speak more about his concept (konsepsi) of a new system of government. In 1957, the era of Liberal Democracy came to an end with the establishment of Guided Democracy. Indonesia would have to wait until 1999 for its next free national elections
+						</p>
+					</div>
+				</section>
+				<section data-background-image={lmao} />
+				<section data-background-image="https://media1.tenor.com/m/PM1NWm8OIyoAAAAC/waiting.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<h2 style="font-size: 4rem; color: black">Kondisi Ekonomi +62 pada era Demokrasi Liberal</h2>
+					<p style="font-size: 3rem; color: beige">Buruknya kondisi ekonomi +62 pada era itu disebabkan oleh:</p>
+					<div class="r-hstack justify-center">
+						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+					</div>
+				</section>
+				<section data-background-image="https://media1.tenor.com/m/PM1NWm8OIyoAAAAC/waiting.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<div class="r-hstack justify-center">
+						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 300px; height: 150px; margin: 20px; color: black">Hutang luar dan dalam negeri</div>
+						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 300px; height: 150px; margin: 10px; color: black">Ketidakstabilan politik di dalam negeri</div>
+						<div data-id="box3" data-auto-animate-delay="0.2" style="background: yellow; width: 300px; height: 250px; margin: 10px; color: black">Ekspor yang hanya bergantung pada 1 sektor sahaja</div>
+					</div>
+					<h2 style="margin-top: 20px; font-size: 4rem; color: black">Kondisi Ekonomi +62 pada era Demokrasi Liberal</h2>
+				</section>
+				<section data-background-image="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjhhdWlnbHdzeGFqMXJ4aG5qMHZnZTdsMG9jcWl5M3BjZ3dxYmJsMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9fcxfQ951fehefos9s/giphy.webp"></section>
+			<!--this is the start of sub-bab 2 pt.2-->
+			</section>
+
 			<section>
 				<section data-background-image="https://media.tenor.com/WUfk54aazc8AAAAM/filthy-frank-dance-filthy-frank.gif" />
 			</section>

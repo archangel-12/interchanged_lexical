216bac04d769f6e4326a05460aff8833bff618f6
the nothing <mydeadass1568@gmail.com>
2024-09-29 14:28:01 +0000

    progress done...

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -12,11 +12,96 @@
 	import lol from './public/hihi.png';
 	import suicidalIdeation from './public/im suicidal.jpeg';
 	import prri from './public/prri.jpg';
+	import Buzz from './public/buzz.jpg';
+	import Criminal from './public/war criminal.jpg';
+	import suicide from './public/suicide.jpg';
 </script>
 
 <Presentation>
 	<div class="reveal">
 		<div class="slides">
+			<!--this is the start of sub-bab 1 pt.1-->
+			<section>
+				<section data-background-image="https://pbs.twimg.com/media/GVlUy37boAA6-QH.jpg">
+					<h1 style="font-size: 4rem; color: black">Indonesia di tengah konstelasi Perang Dingin</h1>
+				</section>
+				<section data-background-image={suicide} />
+				<section data-background-image="https://miro.medium.com/v2/resize:fit:1400/0*u-K6cRlTQc1sVO3W.jpg" />
+				<section data-background-image="https://miro.medium.com/v2/resize:fit:1400/0*u-K6cRlTQc1sVO3W.jpg">
+					<q style="font-size: 3rem; color: black">Berbeda dengan perang tradisional, perang ini tidak melibatkan pertempuran langsung dalam skala besar antara kedua negara adikuasa, tetapi ditandai dengan perang proksi, perlombaan senjata nuklir, spionase, propaganda, dan persaingan ideologis untuk mendapatkan pengaruh global</q>
+				</section>
+				<section data-background-image="https://origins.osu.edu/sites/default/files/inline-images/2023-04-14%20%281%29.png" />
+				<section data-background-image="https://politiikasta.fi/wp-content/uploads/2020/09/INF3-1295-Marshall-Aid-cartoon-2.jpg" />
+				<section data-background-image="https://assets3.cbsnewsstatic.com/hub/i/r/2023/07/11/b0f698b0-b3cb-4a9e-ac73-c43054c3690f/thumbnail/640x360/8c3784fe347c9bf7eff6c8a47f7b7ad1/nato-member-map-7.jpg?v=631cf5f2b4e8db7f9bc428589402864d">
+					<h2 style="font-size: 3rem; color: black;">NATO</h2>
+					<p style="font-size: 3rem; color: black;">North Atlantic Treaty Organization</p>
+				</section>
+				<section data-background-image="https://cdn.britannica.com/62/64262-050-B5A6C575/Cold-War-membership-Europe-Soviet-Union-satellites.jpg">
+					<h2 style="font-size: 4rem; color: black">Pakta Warsawa</h2>
+				</section>
+				<section data-background-image="https://mmc.tirto.id/image/2018/03/10/khruschev-sukarno--life.jpg" />
+				<section data-background-iframe="https://en.wikipedia.org/wiki/Operation_Trikora" data-background-interactive />
+			</section>
+			<section>
+				<section data-background-image="https://i.pinimg.com/originals/e7/ef/67/e7ef6791661662b108f2d5e7a4118e00.gif">
+					<h1 style="font-size: 4rem;">Teknologi</h1>
+					<p style="color: black;">Industri Peroketan</p>
+				</section>
+				<section data-background-image="https://mf.b37mrtl.ru/rbthmedia/images/2019.03/article/5c9cbf9f85600a18d643c492.jpg">
+					<h1 style="color: black; font-size: 4rem">Юрий Алексеевич Гагарин</h1>
+					<p style="color: black">Yuri Alexyevich Gagarin</p>
+				</section>
+				<section data-background-image="https://space.skyrocket.de/img_sat/sputnik-1__1.jpg">
+					<h1 style="color: whitesmoke; font-size: 4rem">Sputnik 1</h1>
+				</section>
+				<section data-background-image={Buzz}>
+					<div style="position: left; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>The Eagle has Landed 🥳🥳🥳</h2>
+						<p>Ini adalah foto dari astronot Buzz Aldrin hormat ke bendera Amerika di Bulan. Buzz Aldrin tergabung dalam misi Apollo 11, yang dimana ia beserta Astronot Neil Armstrong menjadi manusia pertama yang menginjakkan kaki di Bulan</p>
+					</div>
+				</section>
+				<section data-background-image="https://www.popsci.com/wp-content/uploads/2019/07/19/RX4EZMET4KZLMC3O6CTFVXN5KQ.jpg">
+					<div style="position: left; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<p>Dan ini adalah jejak sepatu yang ditinggalkan oleh astronot di misi yang sama, Apollo 11</p>
+					</div>
+				</section>
+				<section data-background-image="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDZ3bHhqYTRuOHRobmEwNmN0N2MyeXdobWNnc2M3M3pmeWh6cHo4NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3JUkOHsbMY7Wy3s3AA/giphy.webp">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>Saturn 5</h2>
+						<p>Ini adalah roket terbesar yang dibuat oleh NASA. Yaitu Saturn 5. Roket ini yang membawa astonot Buzz Aldrin, Neil Armstrong menuju ke Bulan</p>
+					</div>
+				</section>
+				<section data-background-image="https://cdn.mos.cms.futurecdn.net/jt3aonWDALfwfisswSMprk.jpg">
+					<div style="position: justified; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>Ракета-носитель</h2>
+						<p>Dan ini, adalah roket N-1. Ini adalah roket yang seharusnya menjadi kompetitor Saturn 5. Pemerintah Soviet memutuskan untuk menghentikan pengembangan roket ini dikarenakan roket ini mengalami kecacatan dalam hal penerbangan</p>
+					</div>
+				</section>
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg" />
+				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg">
+					<h1 style="font-size: 4rem; color: white">Perang Dingin kenapa ada hubungannya dengan Anjing?</h1>
+				</section>
+				<section data-background-image="https://media.newyorker.com/photos/59fb65a52d0aaf4a81445779/master/w_2560%2Cc_limit/Wellerstein-Laika.jpg">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2>лайка</h2>
+						<p>Perkenalkan, Laika. Laika adalah seekor anjing jalanan yang diambil oleh ilmuwan Soviet di jalanan kota Moskow. Laika dijadikan sebagai subjek eksperimen yang dilakukan oleh ilmuwan Soviet untuk mengetahui seberapa lama hewan bisa bertahan hidup di luar angkasa. Alhasil, hasil tes yang dilakukan sangatlah memuaskan bagi Ilmuwan Soviet, Laika bisa bertahan hidup di luar angkasa (dengan bantuan tabung oksigen dan anesthesia) selama 4 jam. Akan tetapi Laika harus mendekam di kabin Sputnik 2 yang sempit. Laika mengalami depresi berat dan harus menghembuskan nafas terakhir karena depresi berat dan juga mati terbakar. Mati terbakar karena kabin Sputnik 2 tidak bisa membendung panasnya atmosfer bumi</p>
+					</div>
+				</section>
+			</section>
+			<section>
+				<section data-background-image="https://repository.tcu.edu/bitstream/handle/116099117/19338/1979.06.19.tif.preview.jpg?sequence=3&isAllowed=y">
+					<h1 style="font-size: 4rem; color: yellow">Perjanjian SALT 1 & 2</h1>
+				</section>
+				<section data-background-image="https://berlin.mfa.gov.az/upload/multiple/1689062866img__0.jpg">
+					<h1 style="font-size: 4rem; color: white">Konferensi Tingkat Tinggi Non-blok</h1>
+				</section>
+				<section data-background-image="https://setkab.go.id/wp-content/uploads/2015/04/Photo-bersama1.jpg">
+					<h1 style="font-size: 4rem; color: white">Konferensi Asia-Afrika</h1>
+				</section>
+				<section data-background-image="https://i.pinimg.com/originals/45/5e/03/455e0346c3288117ae19c659ed5ecec8.gif" />
+			 <!--this is the end of sub-bab 1 pt.1-->
+			</section>
+
 			<section>
 				<!--this is the start of sub-bab 2 pt.1-->
 				<section data-background-image={Speech}>

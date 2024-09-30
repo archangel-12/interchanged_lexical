12e54171e6afb965c815fbc5425efc39f11a92a3
the nothing <mydeadass1568@gmail.com>
2024-09-30 16:36:49 +0000

    removed some of unused material

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
 	import { Presentation } from '@components';
-	import Speech from './public/the lol.jpg';
 	import USI from './public/peta usi.png';
-	import hiya from './public/hiya.jpg';
-	import yes from './public/yes.jpg';
-	import nya from './public/nya.jpg';
-	import lel from './public/lel.jpg';
-	import troll from './public/troll.jpg';
-	import hihi from './public/hihi.jpg';
 	import lmao from './public/lmao.png';
 	import lol from './public/hihi.png';
 	import suicidalIdeation from './public/im suicidal.jpeg';
 	import prri from './public/prri.jpg';
 	import Buzz from './public/buzz.jpg';
-	import Criminal from './public/war criminal.jpg';
 	import suicide from './public/suicide.jpg';
 </script>
 
@@ -79,7 +71,7 @@
 				</section>
 				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg" />
 				<section data-background-image="https://upload.wikimedia.org/wikipedia/en/7/71/Laika_%28Soviet_dog%29.jpg">
-					<h1 style="font-size: 4rem; color: white">Perang Dingin kenapa ada hubungannya dengan Anjing?</h1>
+					<h1 style="font-size: 4rem; color: black">Perang Dingin kenapa ada hubungannya dengan Anjing?</h1>
 				</section>
 				<section data-background-image="https://media.newyorker.com/photos/59fb65a52d0aaf4a81445779/master/w_2560%2Cc_limit/Wellerstein-Laika.jpg">
 					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
@@ -104,7 +96,7 @@
 
 			<section>
 				<!--this is the start of sub-bab 2 pt.1-->
-				<section data-background-image={Speech}>
+				<section data-background-image="https://i.pinimg.com/564x/2d/15/6b/2d156b17bf97864df3f0ab008508e71e.jpg">
 					<h1 style="font-size: 4rem; color: black">Ketersebaran Kekuatan & Identitas Nasional Baru</h1>
 				</section>
 				<section data-background-image={USI}>
@@ -114,89 +106,11 @@
 					</div>
 				</section>
 				<section data-background-image={USI} />
-				<section data-background-image="https://media1.tenor.com/m/LjVFJn-BVE0AAAAd/trump-kamala.gif">
+				<section data-background-image="https://i.pinimg.com/originals/a1/09/2d/a1092da6cb2c681c7d50ddef7635167d.gif">
 					<h1 style="font-size: 4rem; color: black">1</h1>
 					<h2 style="font-size: 4rem; color: white">Perkembangan Politik pada Masa Demokrasi Liberal</h2>
 				</section>
 			</section>
-
-			<section>
-				<section data-background-image="https://civitasbook.com/_z_album_b/d2/m1/p/aaaaadmp89_pahlawan20140305806290.jpg">
-					<h1 style="font-size: 4rem; color: black">Kabinet Natsir</h1>
-				</section>
-				<section data-background-image="https://civitasbook.com/_z_album_b/d2/m1/p/aaaaadmp89_pahlawan20140305806290.jpg">
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Pada masa kabinet inilah pihak Indonesia dan Belanda berunding perihal Papua pada 4 Desember 1950. Akan tetapi kabinet ini tidak berumur panjang dikarenakan mosi tidak percaya oleh anggota parlemen dari Dewan Konstituante. Dan juga ditambah adanya berbagai macam pemberontakan yang terjadi</p>
-					</div>
-				</section>
-				<section data-background-image={hiya}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Sukiman</h1>
-				</section>
-				<section data-background-image={hiya}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini tidak bertahan lama karena tidak sesuai dengan prinsip politik yang dianut oleh bangsa kita yaitu politik bebas aktif. Ini bisa disebabkan dengan adanya nota kerjasama dari pemerintah Amerika Serikat yang dimana pemerintah Amerika Serikat memberikan bansos berupa bantuan persenjataan militer dan ekonomi kepada rezim orde lama</p>
-					</div>
-				</section>
-				<section data-background-image={yes}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Wilopo</h1>
-				</section>
-				<section data-background-image={yes}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini menjadi satu dari kabinet terkenal karena kabinet ini diisi oleh orang-orang yang memiliki keahlian tertentu di bidangnya</p>
-					</div>
-				</section>
-				<section data-background-image={nya}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Ali Sastromijoyo 1</h1>
-				</section>
-				<section data-background-image={nya}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini terkenal akan keberhasilannya karena kabinet ini salah satu dari pemrakarsa konferensi</p>
-					</div>
-				</section>
-				<section data-background-image="https://backpanel.kemlu.go.id/sites/pusat/PublishingImages/Tentang%20Kami/Museum%20Konferensi%20Asia%20Afrika_jpg.jpg" />
-				<section data-background-image={nya}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini tidak bertahan lama dikarenakan adanya kasus malpraktik dan korupsi yang merajalela sehingga kabinet ini mundur dari pemerintahan</p>
-					</div>
-				</section>
-				<section data-background-image={lel}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Burhanuddin Harahap</h1>
-				</section>
-				<section data-background-image={lel}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>
-							Kabinet ini terkenal akan pemilihan anggota DPR dan juga pemilihan anggota konstituante. Indonesia di era ini juga berhasil memutus hubungan dengan Belanda melalui Uni Indonesia-Belanda di bawah Ratu Juliana sebagai pemimpinnya.
-							Kabinet ini dianggap sebagai satu-satunya kabinet yang berhasil mengemban tugasnya karena tugas-tugas yang dikerjakan berhasil diselesaikan oleh kabinet ini
-						</p>
-					</div>
-				</section>
-				<section data-background-image={nya}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Ali Sastromijoyo 2</h1>
-				</section>
-				<section data-background-image={nya}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini terkenal dikarekan konflik internal. Konflik internal disini ialah sang Perdana Menteri berpendapat bahwasannya kabinet ini tidak harus mengembalikan mandatnya ke Presiden hanya karena desakan dari daerah. Kabinet ini terkenal karena tidak mematuhi isi dari KMB karena Belanda tidak bersedia untuk melepas Papua</p>
-					</div>
-				</section>
-				<section data-background-image={troll}>
-					<h1 style="font-size: 4rem; color: black">Kabinet Juanda</h1>
-				</section>
-				<section data-background-image={troll}>
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Kabinet ini terkenal karena pertama kali mendefinisikan Deklarasi Juanda yang dimana...</p>
-					</div>
-				</section>
-				<section data-background-image={hihi} />
-				<section data-background-image="https://ichef.bbci.co.uk/images/ic/1024x576/p09x75rz.jpg">
-					<h1 style="font-size: 4rem; color: grey">Gerwani</h1>
-				</section>
-				<section data-background-image="https://ichef.bbci.co.uk/images/ic/1024x576/p09x75rz.jpg">
-					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
-						<p>Pada awalnya, organisasi ini terdiri dari aktivis perempuan yang dimana ingin menyuarakan hak-hak perempuan agar mendapat perlakuan setara seperti laki-laki. Mungkin pada awalnya, organisasi ini mendapat dukungan luas dari masyarakat, akan tetapi mengingat organisasi memiliki hubungan dengan PKI, organisasi ini dibungkam dan beberapa anggota nya berakhir dipenjara di kamp konsentrasi era orde baru</p>
-					</div>
-				</section>
-			</section>
-
 			<section>
 				<section data-background-image="https://images.bisnis.com/upload/img/demo_RUU_Pilkada_di_DPR.jpg">
 					<h1 style="font-size: 4rem; color: black">Pemilihan Umum</h1>
@@ -211,41 +125,6 @@
 					</div>
 				</section>
 				<section data-background-image={lmao} />
-				<section data-background-image="https://media1.tenor.com/m/PM1NWm8OIyoAAAAC/waiting.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-					<h2 style="font-size: 4rem; color: black">Kondisi Ekonomi +62 pada era Demokrasi Liberal</h2>
-					<p style="font-size: 3rem; color: beige">Buruknya kondisi ekonomi +62 pada era itu disebabkan oleh:</p>
-					<div class="r-hstack justify-center">
-						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-					</div>
-				</section>
-				<section data-background-image="https://media1.tenor.com/m/PM1NWm8OIyoAAAAC/waiting.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-					<div class="r-hstack justify-center">
-						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 300px; height: 150px; margin: 20px; color: black">Hutang luar dan dalam negeri</div>
-						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 300px; height: 150px; margin: 10px; color: black">Ketidakstabilan politik di dalam negeri</div>
-						<div data-id="box3" data-auto-animate-delay="0.2" style="background: yellow; width: 300px; height: 250px; margin: 10px; color: black">Ekspor yang hanya bergantung pada 1 sektor sahaja</div>
-					</div>
-					<h2 style="margin-top: 20px; font-size: 4rem; color: black">Kondisi Ekonomi +62 pada era Demokrasi Liberal</h2>
-				</section>
-				<section data-background-image="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjhhdWlnbHdzeGFqMXJ4aG5qMHZnZTdsMG9jcWl5M3BjZ3dxYmJsMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9fcxfQ951fehefos9s/giphy.webp" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-					<h2 style="font-size: 4rem;">Usaha untuk memperbaiki Perekonomian</h2>
-					<div class="r-hstack justify-center">
-						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box4" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-					</div>
-				</section>
-				<section data-background-image="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjhhdWlnbHdzeGFqMXJ4aG5qMHZnZTdsMG9jcWl5M3BjZ3dxYmJsMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9fcxfQ951fehefos9s/giphy.webp" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
-					<div class="r-hstack justify-center">
-						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 250px; height: 100px; margin: 10px;">Gunting Syarifuddin</div>
-						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 250px; height: 100px; margin: 10px;">Gerakan Benteng</div>
-						<div data-id="box3" data-auto-animate-delay="0.2" style="background: yellow; width: 280px; height: 130px; margin: 10px;">Nasionalisasi De Javasche Bank</div>
-						<div data-id="box4" data-auto-animate-delay="0.2" style="background: grey; width: 250px; height: 100px; margin: 10px;">Sistem Ali-Baba</div>
-					</div>
-					<h2 style="margin-top: 20px;">Usaha untuk memperbaiki Perekonomian</h2>
-				</section>
 				<section data-background-image="https://mmc.tirto.id/image/otf/640x0/2019/03/16/sukarno-and-aidit--1--associated-press_ratio-16x9.jpg">
 					<h1 style="font-size: 4rem;">Nasakom</h1>
 				</section>
@@ -277,10 +156,11 @@
 			<section>
 				<!--this is the start of sub-bab 2 pt.4-->
 				<section></section>
+				<!--this is the end of sub-bab 2 pt.4-->
 			</section>
 
 			<section>
-				<section data-background-image="https://media.tenor.com/WUfk54aazc8AAAAM/filthy-frank-dance-filthy-frank.gif" />
+				<section data-background-video="https://v1.pinimg.com/videos/mc/720p/0f/f6/39/0ff639354f4245d35b44e41847b0d4b1.mp4" data-background-video-loop data-background-video-muted />
 			</section>
 		</div>
 	</div>
